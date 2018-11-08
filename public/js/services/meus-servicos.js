29841d5b26e9199b73c8adbc183a65764b9377ee
Leandro de Freitas <LFreitas@raiadrogasil.com>
2018-11-08 17:12:26 -0200

    Removido service

--- public/js/services/meus-servicos.js
+++
@@ -1,9 +0,0 @@
-angular.module('meusServicos', ['ngResource'])
-	.factory('recursoPedido', function($resource) {
-
-		return $resource('/v1/status', null, {
-			'update' : { 
-				method: 'PUT'
-			}
-		});
-	});
