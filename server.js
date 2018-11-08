c636f36474d33f19f919985f56e708af5ac8c8a8
Leandro de Freitas <LFreitas@raiadrogasil.com>
2018-11-08 20:30:24 -0200

    Atualização da porta padrão

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var http = require('http')
     ,app = require('./config/express')
     db = require('./config/database');
 
-http.createServer(app).listen(3000, function() {
+var port = process.env.PORT || 8080;    
+
+http.createServer(app).listen(port, function() {
     console.log('Servidor escutando na porta: ' + this.address().port);
 });
 
