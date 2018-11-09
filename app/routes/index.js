5edac65bcde2826cea8081b8b731a7a1b2743106
lfreitas <lfreitas.mazzatech@rd.com.br>
2018-11-09 09:13:58 -0200

    Add. script

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,8 +5,7 @@ module.exports  = function(app) {
 
     app.get('/v1/statusPorPedido', api.busca);   
     app.get('/v1/pedido', api.listaStatus);
-
-       
+     
     // habilitando HTML5MODE
     app.all('/*', function(req, res) {
         res.sendFile(path.resolve('public/index.html'));
